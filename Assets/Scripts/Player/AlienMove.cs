de0689a299444cd6a28697babd0d2dc2674bf20b
Maddi <maddi.fitzmaurice@gmail.com>
2022-07-03 21:15:31 +1000

    AI Update 1
    - Added navmesh

---
+++ Assets/Scripts/Player/AlienMove.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AlienMove : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    private NavMeshAgent navMeshAgent;
+
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        navMeshAgent.destination = target.position;
+    }
+}
